b4f1e4d4215d2d3d0d3f046840933d7ebdf7fb9c
Stanislav Kovtun <sstanisslaw@gmail.com>
2023-04-16 23:18:06 +0300

    Added Redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 // Add store creation, root reducer
 
-import { combineReducers, legacy_createStore } from 'redux';
+import {
+	combineReducers,
+	legacy_createStore,
+	compose,
+	applyMiddleware,
+} from 'redux';
+import ReduxThunk from 'redux-thunk';
+
 import userReducer from './user/reducer';
 import authorReducer from './authors/reducer';
 import coursesReducer from './courses/reducer';
 
 const store = legacy_createStore(
 	combineReducers({ userReducer, authorReducer, coursesReducer }),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(
+		applyMiddleware(ReduxThunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;
